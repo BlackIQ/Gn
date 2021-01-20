c3161836507dada743c9295581550f17d3b161d3
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-01-20 09:29:00 +0330

    Make Python file

---
+++ main.py
@@ -0,0 +1,15 @@
+# Gn App , Music Runner , In Python language
+
+from time import strftime, localtime
+from os import system
+
+t = input("Time : ")
+
+print("App is running !\n")
+
+while True:
+    if strftime('%H : %M : %S', localtime()) == t:
+        print(f"Wake up body ! it's {t} !")
+        system('mpg123 ~/morning/Music/*.mp3')
+    else:
+        pass
