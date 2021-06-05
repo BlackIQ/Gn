25b6bc005f96a464df5368b8a1e241afddca0659
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-06-05 11:11:27 +0430

    Fix C file

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 // Gn App . Python File Runner , In C language .
 
+#include <stdio.h>
+#include <stdlib.h>
+
 void main() {
 	printf("Hello , Welcome to Gn app !\n\n");
 	system("python3 ~/Gn/main.py");
